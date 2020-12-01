0437ea4f5ecd035c73252444cf2514441e5c480c
Mely07 <damely.tineo07@gmail.com>
2020-11-30 21:33:06 -0500

    Set up signup form on home page. Began front-end auth process.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_230131) do
+ActiveRecord::Schema.define(version: 2020_11_29_223236) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "likes"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_230131) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
