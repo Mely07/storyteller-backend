474ebb547fc3dace253a66fd196a8e710c290965
Mely07 <damely.tineo07@gmail.com>
2020-12-06 23:35:22 -0500

    Set up user model validations and  signup/login error messages display.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates_presence_of :username, :email, :password
+    validates_uniqueness_of :username, :email
+    validates :password, length: { in: 4..10 }
 end
