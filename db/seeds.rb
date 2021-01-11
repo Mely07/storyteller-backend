a09687b48ca896c8cd07310b0d3280d1712f16b1
Mely07 <damely.tineo07@gmail.com>
2021-01-10 22:25:12 -0500

    Updated CORS.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,4 @@
 Story.create(opening_line: 'Once upon a time...', image: 'https://www.writersedit.com/wp-content/uploads/2016/06/Fantasy-elements_3.jpg', genre: 'fantasy', author: 'Me')
 
 Comment.create(likes: 0, dislikes: 0, funny_rating: 0, scary_rating: 0, story_id: 1, author: 'not sure', text: 'this is a comment')
-Comment.create(likes: 0, dislikes: 0, funny_rating: 0, scary_rating: 0, story_id: 2, author: 'not sure', text: 'this is comment no. 2')
-
+Comment.create(likes: 0, dislikes: 0, funny_rating: 0, scary_rating: 0, story_id: 2, author: 'not sure', text: 'this is comment no. 2
