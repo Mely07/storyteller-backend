5c6fd7745cd777583cf205cdf0c07d6084295b66
Mely07 <damely.tineo07@gmail.com>
2021-01-19 23:04:41 -0500

    Testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://thestorytellerapplication.netlify.app'
-    
+    origins 'https://thestorytellerapplication.netlify.app', 'http://localhost:3000'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
